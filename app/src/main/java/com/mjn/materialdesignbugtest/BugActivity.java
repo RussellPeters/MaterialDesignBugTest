60f5151be7882bb6e4ca166acef33fc694f9c01d
mengjingnan <mengjingnan@58.com>
2016-01-21 15:19:22 +0800

    DEV: bug state

---
+++ app/src/main/java/com/mjn/materialdesignbugtest/BugActivity.java
@@ -0,0 +1,30 @@
+package com.mjn.materialdesignbugtest;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by mjn on 2016/1/21.
+ */
+public class BugActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
